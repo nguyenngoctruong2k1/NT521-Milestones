9a9f64f2e6b84e6ac788ecce7d93c24426beba93
Truong Nguyen <2k1.nguyenngoctruong@gmail.com>
2021-12-14 17:14:44 +0700

    test-DC-2

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package nl.amis.smeetsm.demoservice;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoserviceApplicationTests {
+class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
